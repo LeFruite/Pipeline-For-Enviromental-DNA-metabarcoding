0853be05fbb214ef02b8e97592f10fdb44666b09
LeFruite <109670201+LeFruite@users.noreply.github.com>
2026-04-22 17:31:24 +0300

    Update Clusterization.sh

--- Clusterization.sh
+++ Clusterization.sh
@@ -6,3 +6,14 @@ speconsense input.fastq -O clust_results/ --threads 8
 
 # Merge clusters with >= 99% identity (1% dissimilarity)
 speconsense-summarize --source . --summary-dir MyResults --min-identity 0.99
+
+# Install via Conda
+conda install -c bioconda taxonkit
+
+# Download and set up the NCBI taxonomy data (approx 400MB)
+wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+tar -zxvf taxdump.tar.gz
+mkdir -p ~/.taxonkit
+mv names.dmp nodes.dmp delnodes.dmp merged.dmp ~/.taxonkit
+
+taxonkit lineage -i 7 blast_results.tsv | taxonkit reformat -i 11 -f "{k};{p};{c};{o};{f};{g};{s}" > blast_with_taxonomy.tsv
